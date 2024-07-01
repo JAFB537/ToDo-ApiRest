21556bc7863837962803651f6245b170934dac68
JAFB537 <jafb537@gmail.com>
2024-07-01 16:13:20 -0600

    Update Project Schema

--- src/schemas/project.schemas.js
+++ src/schemas/project.schemas.js
@@ -3,8 +3,8 @@ import { z } from "zod";
 const projectSchema = z.object({
   Name: z.string().min(1, { message: "Project name is required" }).max(50),
   Description: z.string().max(1000).optional(),
-  StartDate: z.date().nullable(),
-  EndDate: z.date().nullable(),
+  StartDate: z.date().or(z.string()).nullable(),
+  EndDate: z.date().or(z.string()).nullable(),
   StateID: z.number().int(),
   UserID: z.string().uuid(),
 });
